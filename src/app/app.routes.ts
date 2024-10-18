e7da5f09823e05b8ee063d62307e5646666158b7
luis reis <luis.reis@starfusion.com.br>
2024-10-18 11:07:58 -0300

    Base do projeto, criação dos componentes e modulos iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+
+  {
+    path: "login",
+    loadChildren: () =>
+      import("../app/modules/login/login.module").then((m) => m.LoginModule),
+  },
+  {
+    path: "register",
+    loadChildren: () =>
+      import("../app/modules/register/register.module").then(
+        (m) => m.RegisterModule
+      ),
+  },
+  {
+    path: "dashboard",
+    loadChildren: () =>
+      import("../app/modules/dashboard/dashboard.module").then(
+        (m) => m.DashboardModule
+      ),
+  },
+];
---
+++ src/app/components/dashboard-grid/dashboard-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-grid.component.html',
+  styleUrl: './dashboard-grid.component.scss'
+})
+export class DashboardGridComponent {
+
+}
